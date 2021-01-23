d1177ab8d14a6651637bfc6918b53bbcf4c30806
Amin <amin@lit.codes>
2021-01-23 23:17:56 +0800

    Add chartlib frontend and backend

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# infrastructure
-IaC that we use to host LitCodes apps
+# chartlib
+
+## backend
+
+The node application that is responsible for authentication and everything else
+except for the Graphql API.
+
+## frontend
+
+Everything on the frontend side of things, can be built and served statically.
---
+++ backend/schema/Department.js
@@ -0,0 +1,31 @@
+cube(`Department`, {
+  sql: `SELECT * FROM public.department`,
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [id, name]
+    }
+  },
+  
+  dimensions: {
+    id: {
+      sql: `id`,
+      type: `number`,
+      primaryKey: true
+    },
+    
+    name: {
+      sql: `name`,
+      type: `string`
+    }
+  },
+
+  refreshKey: {
+    every: `4 week`
+  }
+});
